43d064ac32df5440303710afaa9be7c04d25e80b
Felipe Passos <passos.fe@gmail.com>
2019-11-06 17:36:09 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./App.css";
+
+import profile from "./assets/profile.jpeg";
+
+function App() {
+  return <img width="200" src={profile}></img>;
+}
+
+export default App;
